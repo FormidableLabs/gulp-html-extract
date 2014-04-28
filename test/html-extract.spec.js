6794d9a4d73e5c45a339b78ea924becae6cbe27d
Ryan Roemer <ryan.roemer@formidablelabs.com>
2014-04-28 08:06:31 -0700

    More tests

--- test/html-extract.spec.js
+++ test/html-extract.spec.js
@@ -17,28 +17,49 @@ describe("html-extract", function () {
     });
   });
 
-  it("should handle no matches", function (done) {
-    var stream = extract(),
+  it("should handle no matches with custom selectors", function (done) {
+    var stream = extract({ sel: ".NO_MATCH" }),
+      count = 0,
       err;
-
 
     stream
       .on("data", function () {
-        console.log("TODO HERE HI", arguments);
+        count++;
       })
       .on("error", function (err) {
         err = err;
       })
       .on("end", function () {
         expect(err).to.not.be.ok;
+        expect(count).to.equal(0);
         done(err);
       })
       .end(this.file);
   });
 
-  it("should handle no matches with custom selectors");
+  it("should extract scripts by default", function (done) {
+    var stream = extract(),
+      count = 0,
+      err;
 
-  it("should extract scripts by default");
+    stream
+      .on("data", function (file) {
+        count++;
+        expect(file.path.toString()).to.contain("-script");
+        expect(file.contents.toString()).to.contain("var ");
+      })
+      .on("error", function (err) {
+        err = err;
+      })
+      .on("end", function () {
+        expect(err).to.not.be.ok;
+        expect(count).to.equal(2);
+        done(err);
+      })
+      .end(this.file);
+  });
 
-  it("should extract custom selectors");
+  it.skip("should extract custom selectors", function () {
+
+  });
 });
