afb182af15476eefacc0c9bcba70672660bb40c9
Ryan Roemer <ryan@loose-bits.com>
2015-10-01 07:45:45 -0700

    Add more tests, confirm fixes #1

--- test/html-extract.spec.js
+++ test/html-extract.spec.js
@@ -6,24 +6,72 @@ var fs = require("fs"),
 
   gutil = require("gulp-util"),
   extract = require("../html-extract"),
-  html = fs.readFileSync(path.join(__dirname, "test.html"), "utf8");
+  html = fs.readFileSync(path.join(__dirname, "test.html"), "utf8"),
+
+  file,
+  count,
+  err;
+
+// Helpers
+var expectEmpty = function (file, done) {
+  extract()
+    .on("data", function () {
+      count++;
+    })
+    .on("error", function (err) {
+      err = err;
+    })
+    .on("end", function () {
+      expect(err).to.not.be.ok;
+      expect(count).to.equal(0);
+      done(err);
+    })
+    .end(file);
+};
 
 describe("html-extract", function () {
 
   beforeEach(function () {
     // Fake file.
-    this.file = new gutil.File({
+    file = new gutil.File({
       path: "test/test.html",
       contents: new Buffer(html)
     });
+
+    count = 0;
+    err = null;
   });
 
-  it("should handle no matches with custom selectors", function (done) {
-    var stream = extract({ sel: ".NO_MATCH" }),
-      count = 0,
-      err;
+  it("handles an empty string", function (done) {
+    file = new gutil.File({
+      path: "test/test.html",
+      contents: new Buffer("")
+    });
 
-    stream
+    expectEmpty(file, done);
+  });
+
+
+  it("handles bad HTML", function (done) {
+    file = new gutil.File({
+      path: "test/test.html",
+      contents: new Buffer("<html_OMG_THIS_ISBAD</BAD>")
+    });
+
+    expectEmpty(file, done);
+  });
+
+  it("handles no body", function (done) {
+    file = new gutil.File({
+      path: "test/test.html",
+      contents: new Buffer("<html></html>")
+    });
+
+    expectEmpty(file, done);
+  });
+
+  it("handles no matches with custom selectors", function (done) {
+    extract({ sel: ".NO_MATCH" })
       .on("data", function () {
         count++;
       })
@@ -35,15 +83,11 @@ describe("html-extract", function () {
         expect(count).to.equal(0);
         done(err);
       })
-      .end(this.file);
+      .end(file);
   });
 
-  it("should extract scripts by default", function (done) {
-    var stream = extract(),
-      count = 0,
-      err;
-
-    stream
+  it("extracts scripts by default", function (done) {
+    extract()
       .on("data", function (file) {
         count++;
         expect(file.path).to.contain("-script");
@@ -57,15 +101,11 @@ describe("html-extract", function () {
         expect(count).to.equal(3);
         done(err);
       })
-      .end(this.file);
+      .end(file);
   });
 
-  it("should extract custom selectors", function (done) {
-    var stream = extract({ sel: "textarea" }),
-      count = 0,
-      err;
-
-    stream
+  it("extracts custom selectors", function (done) {
+    extract({ sel: "textarea" })
       .on("data", function (file) {
         count++;
         expect(file.path).to.contain("-textarea");
@@ -79,15 +119,11 @@ describe("html-extract", function () {
         expect(count).to.equal(2);
         done(err);
       })
-      .end(this.file);
+      .end(file);
   });
 
-  it("should match id", function (done) {
-    var stream = extract({ sel: "#first-textarea" }),
-      count = 0,
-      err;
-
-    stream
+  it("matches id", function (done) {
+    extract({ sel: "#first-textarea" })
       .on("data", function (file) {
         var text = file.contents.toString().replace(/^\s*|\s*$/g, "");
 
@@ -103,6 +139,6 @@ describe("html-extract", function () {
         expect(count).to.equal(1);
         done(err);
       })
-      .end(this.file);
+      .end(file);
   });
 });
