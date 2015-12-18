91a56adf835ac4f64667580b350d4b5762053d52
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-12-18 02:31:59 -0800

    Add 'strip' option to dedent text to first found real character.
    * Adds tests
    * Reorgs tests into describe blocks
    * Replaces gulfile with mocha, eslint on command line

--- html-extract.js
+++ html-extract.js
@@ -1,32 +1,34 @@
+"use strict";
+
 /**
  * Extract HTML text.
  */
-var cheerio = require("cheerio"),
-  gutil = require("gulp-util"),
-  PluginError = gutil.PluginError,
-  through2 = require("through2"),
-  PLUGIN_NAME = "html-text";
+var cheerio = require("cheerio");
+var gutil = require("gulp-util");
+var PluginError = gutil.PluginError;
+var through2 = require("through2");
+var stripIndent = require("./lib/strip-indent");
+var PLUGIN_NAME = "html-text";
 
 /**
  * ## `html-extract`
  *
  * Extract text from HTML.
  *
- * @param {Object} opts     Options
- * @param {String} opts.sel Element selector [Default: `script`] (_optional_)
+ * @param {Object} opts       Options
+ * @param {String} opts.sel   Element selector [Default: `script`]
+ * @param {String} opts.strip Strip to indent level [Default: `false`]
+ * @returns {void}
  */
 module.exports = function (opts) {
   opts = opts || {};
   var sel = opts.sel || "script";
+  var strip = !!opts.strip;
 
   var stream = through2.obj(function (file, enc, callback) {
+    /*eslint-disable no-invalid-this*/
     var self = this;
-    var contentExtracted;
-    var els;
-
-    function hasChildren(el) {
-      return el.children.length > 0;
-    }
+    /*eslint-enable no-invalid-this*/
 
     if (file.isStream()) {
       return stream.emit("error",
@@ -34,18 +36,28 @@ module.exports = function (opts) {
     }
 
     if (file.isBuffer()) {
-      contentExtracted = cheerio.load(file.contents.toString("utf8"));
-      els = contentExtracted(sel);
+      var contentExtracted = cheerio.load(file.contents.toString("utf8"));
+      var els = contentExtracted(sel);
+
       [].forEach.call(els, function (el, i) {
-        if (hasChildren(el)) {
+        // Process children if we have them.
+        if (el.children.length > 0) {
+          var data = el.children[0].data;
+
+          // Strip to indentation of first real line if specified
+          if (strip) {
+            data = stripIndent(data);
+          }
+
           self.push(new gutil.File({
             // Name: id or tag + index.
             path: file.path + "-" + (el.attribs.id || el.tagName + "-" + i),
-            contents: new Buffer(el.children[0].data)
+            contents: new Buffer(data)
           }));
         }
       });
-      callback();
+
+      return callback();
     }
   });
 
